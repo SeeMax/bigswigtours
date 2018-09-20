2f9b9346bd226ed6420ad89c538a7cbf5e813881
Max Mearsheimer <max@seemaxwork.com>
2018-09-20 15:49:44 -0500

    Header update

--- wp-content/themes/fug_theme/header.php
+++ wp-content/themes/fug_theme/header.php
@@ -1,6 +1,7 @@
 <!doctype html>
 <html <?php language_attributes(); ?> class="no-js loader-class">
 <head>
+	<!-- Global site tag (gtag.js) - Google Analytics -->
 	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-52601638-1"></script>
 	<script>
 	  window.dataLayer = window.dataLayer || [];
@@ -9,13 +10,14 @@
 
 	  gtag('config', 'UA-52601638-1');
 	</script>
+
 	<meta charset="<?php bloginfo('charset'); ?>">
 	<title><?php wp_title( '' ); ?><?php if ( wp_title( '', false ) ) { echo ' : '; } ?><?php bloginfo( 'name' ); ?> | <?php the_title(); ?></title>
 
 	<link href="//www.google-analytics.com" rel="dns-prefetch">
   <link href="<?php echo get_template_directory_uri(); ?>/img/bigswig_fav.png" rel="shortcut icon">
    <!-- <link href="<?php echo get_template_directory_uri(); ?>/img/icons/touch.png" rel="apple-touch-icon-precomposed"> -->
-
+	<meta name="google-site-verification" content="0R72ABSc6dta_F7U47lrnnc361hJ08VtvzM0zTnuQ2A" />
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta name="description" content="<?php bloginfo('description'); ?>">
@@ -51,6 +53,6 @@
 				<span class="hamMid"></span>
 				<span class="hamBot"></span>
 			</div>
-		
+
 		</div>
 	</header>
